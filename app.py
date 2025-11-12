6c691df653e664295dd4649c558070dbee6c35d1
ishu34311-maker <ishu34311@gmail.com>
2025-11-12 15:26:04 +0530

    Refactor app.py to include MongoDB connection

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#import pymongo
+import pymongo
 from datetime import date
 
 # ---------------------- CONFIGURATION ----------------------
@@ -155,4 +155,5 @@ elif menu == "User Query":
                 st.success("✅ Your query has been submitted successfully!")
             else:
                 st.warning("⚠️ Please fill all required fields (Name, Email, and Query).")
+
 
